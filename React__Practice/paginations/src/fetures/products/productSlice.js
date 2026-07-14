009ba7abda6b14baef31d23129638641b4a6092a
tarun-102 <tarungohil6969@gmail.com>
2026-07-14 16:09:22 +0530

    fix formation

--- React__Practice/paginations/src/fetures/products/productSlice.js
+++ React__Practice/paginations/src/fetures/products/productSlice.js
@@ -9,14 +9,12 @@ const initialState = {
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
   async () => {
-    const response = await fetch(
-      "https://dummyjson.com/products?limit=100"
-    );
+    const response = await fetch("https://dummyjson.com/products?limit=100");
 
     const data = await response.json();
 
     return data.products;
-  }
+  },
 );
 
 const productSlice = createSlice({
